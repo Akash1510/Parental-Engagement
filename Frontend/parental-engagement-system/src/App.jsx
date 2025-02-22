310f12818c52fbe3d55086ebb7d1dab8c0398b11
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2025-02-22 22:32:17 +0530

    first commit

---
+++ Frontend/parental-engagement-system/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-cyan-900'>
+           <p className='text-red-300'>Hello mY dear Akash</p>
+    </div>
+    </>
+  )
+}
+
+export default App
